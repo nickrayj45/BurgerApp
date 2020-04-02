e4ee350d5dcb527a299b4fe086751b15293c64e7
Nicholas Jaquez <nrjaquez@gmail.com>
2020-04-01 22:39:17 -0700

    commit w/orm

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,31 @@
 var orm = require("./connection.js");
 
+// Functions needed
 var orm = {
+    // SelectAll to grab info from table
     selectAll: function(tableInput) {
         var queryString = "SELECT * FROM ??";
         orm.query(queryString, [tableInput])
             if (err) throw err;
             console.log(result);
-    }
+    },
+    // InsertOne to insert values into columns
+    insertOne: function(tableInput, col1, col2, val1, val2){
+        var queryString = "INSERT INTO ??,(??, ??), VALUES(??, ??)";
+        orm.query(queryString, [tableInput, burgers, devoured])
+        if (err) throw err;
+        console.log(result);
+    },
+    // UpdateOne to update values in columns of the table
+    updateOne: function(tableInput, col1, val1, col2, val2){
+        var queryString = "UPDATE ?? SET ?? = ??, WHERE ?? = ??";
+        orm.query(queryString, [tableInput, col1, val1, col2, val2])
+        if (err) throw err;
+        console.log(result);
 
-    
+    }
 }
-insertOne()
-updateOne()
+
+
 
 module.exports = orm
--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-// Dependencies
 var express = require("express");
-var mysql = require("mysql");
 
-// Create instance of express app.
+var PORT = process.env.PORT || 4000;
+
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers"
-});
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Initiate MySQL Connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_Controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
